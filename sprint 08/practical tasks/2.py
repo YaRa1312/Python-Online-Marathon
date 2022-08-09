4dce7fce6f7705cdbd04ca7e4c316ee3b6d3cc6c
Iryna Pleshyvtseva <superfrench041@gmail.com>
2022-08-09 16:10:49 +0300

    added sprint 08 task 3

--- sprint 08/practical tasks/2.py
+++ sprint 08/practical tasks/2.py
@@ -4,48 +4,43 @@
 
 ########################################## 2 ##########################################
 ################################ PASSED ################################
-# Please, write the code with unit tests for the function "divide":
-# minimum 3 tests
-# must chek all flows
-# all test must be pass
-# no failures
-# no skip
-
 import unittest
-from parameterized import parameterized
 
 
 def divide(num1, num2):
     return float(num1/num2)
 
 
-# class GoodInstanceInteger:
-#     random_integer_num = 5
+class GoodInstanceInteger:
 
-# class GoodInstanceFloat:
-#     random_float_num = 5.0
+    random_integer_num = 5
 
-class GoodInstances:
-    pass
+
+class GoodInstanceFloat:
+
+    random_float_num = 5.0
+
 
 class DivideTest(unittest.TestCase):
     
-    # @parameterized.expand([
-    #     [5, GoodInstanceInteger, 'num is not integer'],
-    #     [5.0, G]
-    # ])
-    # def test_parameterized(self, checked_object, class_name, message):
-    #     pass
-    # def test_parametrized(self):
-    #     checked_object = GoodInstanceInteger()
-    #     msg = "given object is not instance of class GoodInstance"
-    #     self.assertIsInstance(checked_object, GoodInstanceInteger, msg)
+    def test_type_integer(self):
+        checked_object = GoodInstanceInteger()
+        msg = "given object is not instance of class GoodInstanceInteger"
+        self.assertIsInstance(checked_object, GoodInstanceInteger, msg)
+        
+    def test_type_float(self):
+        checked_object = GoodInstanceFloat()
+        msg = "given object is not instance of class GoodInstanceFloat"
+        self.assertIsInstance(checked_object, GoodInstanceFloat, msg)
     
     def test_exception_zero_division(self):
-        for positive_num in range(1, (1000000**10.5)):
+        for positive_num in range(1, (10**5)):
             self.assertRaises(Exception, divide, positive_num, 0)
-        for negative_num in range(-(1000000**10.5), -1):
+        for negative_num in range(-(10**5), -1):
             self.assertRaises(Exception, divide, negative_num, 0)
+
+    def test_sth(self):
+        self.assertAlmostEqual(divide(1, 6), 0.16666666666666666, 17)
 
 ######################### tests #########################
 
--- sprint 08/practical tasks/3.py
+++ sprint 08/practical tasks/3.py
@@ -3,17 +3,50 @@
 # practical tasks
 
 ########################################## 3 ##########################################
+# Write the function quadratic_equation with arguments a, b ,c that solution to quadratic equation without a complex solution.
+
+# Write unit tests for this function with QuadraticEquationTest class
+
+# Minimum 3 tests: discriminant < 0, discriminant > 0, discriminant = 0
+
 ################################ PASSED ################################
 import unittest
+import math
 
 
 def quadratic_equation(a, b, c):
-    print(a)
-    pass
+    discriminant = (b**2)-(4*a*c)
+    if discriminant < 0:
+        return(None)
+    x1 = (-b+math.sqrt(discriminant))/(2*a)
+    x2 = (-b-math.sqrt(discriminant))/(2*a)
+    if x1 == x2:
+        return x1
+    else:
+        return(x1, x2)
+
+
+test_data_with_positive_discriminant = [2, 1, -1]
+test_data_with_negative_discriminant = [-2, 1, -1]
 
 
 class QuadraticEquationTest(unittest.TestCase):
-    pass
+    
+    def test_positive_discriminant(self):
+        for el in test_data_with_positive_discriminant:
+            discriminant = ((el[1]) ** 2) - (4 * el[0] * el[2])
+        expected = discriminant > 0
+        self.assertTrue(expected, msg="Discriminant is NOT greater than 0")
+
+    def test_negative_discriminant(self):
+        for el in test_data_with_negative_discriminant:
+            discriminant = ((el[1]) ** 2) - (4 * el[0] * el[2])
+        expected = discriminant < 0
+        self.assertTrue(expected, msg="Discriminant is NOT less than 0")
+
+    def test_zero_discriminant(self):
+        self.assertRaises(Exception, quadratic_equation, 0, 0, 0)
+        # pass
 
 ######################### tests #########################
 
