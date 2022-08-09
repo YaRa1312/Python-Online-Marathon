7c8bbe9a79913d30ca82857d6a9569e3675662d7
Iryna Pleshyvtseva <superfrench041@gmail.com>
2022-08-09 16:13:10 +0300

    added final sprint 08 task 3

--- sprint 08/practical tasks/3.py
+++ sprint 08/practical tasks/3.py
@@ -15,38 +15,49 @@ import math
 
 
 def quadratic_equation(a, b, c):
-    discriminant = (b**2)-(4*a*c)
-    if discriminant < 0:
-        return(None)
-    x1 = (-b+math.sqrt(discriminant))/(2*a)
-    x2 = (-b-math.sqrt(discriminant))/(2*a)
-    if x1 == x2:
-        return x1
-    else:
-        return(x1, x2)
+    try:
+        discriminant = (b**2)-(4*a*c)
+        if discriminant < 0:
+            return(None)
+        x1 = (-b+math.sqrt(discriminant))/(2*a)
+        x2 = (-b-math.sqrt(discriminant))/(2*a)
+        if x1 == x2:
+            return x1
+        else:
+            return(x1, x2)
+    except ZeroDivisionError:
+        raise ValueError
 
 
-test_data_with_positive_discriminant = [2, 1, -1]
-test_data_with_negative_discriminant = [-2, 1, -1]
+test_data_with_positive_discriminant = (2, 1, -1)
+test_data_with_negative_discriminant = (-2, 1, -1)
+
+
+class NormInstances:
+    type_int = 5
+    type_float = 5.0
 
 
 class QuadraticEquationTest(unittest.TestCase):
     
     def test_positive_discriminant(self):
-        for el in test_data_with_positive_discriminant:
-            discriminant = ((el[1]) ** 2) - (4 * el[0] * el[2])
+        discriminant = ((test_data_with_positive_discriminant[1]) ** 2) - (4 * test_data_with_positive_discriminant[0] * test_data_with_positive_discriminant[2])
         expected = discriminant > 0
         self.assertTrue(expected, msg="Discriminant is NOT greater than 0")
 
     def test_negative_discriminant(self):
-        for el in test_data_with_negative_discriminant:
-            discriminant = ((el[1]) ** 2) - (4 * el[0] * el[2])
+        discriminant = ((test_data_with_negative_discriminant[1]) ** 2) - (4 * test_data_with_negative_discriminant[0] * test_data_with_negative_discriminant[2])
         expected = discriminant < 0
         self.assertTrue(expected, msg="Discriminant is NOT less than 0")
 
     def test_zero_discriminant(self):
-        self.assertRaises(Exception, quadratic_equation, 0, 0, 0)
-        # pass
+        with self.assertRaises(ZeroDivisionError):
+            0 / 0
+
+    def test_a_b_c_type(self):
+        checked_object = NormInstances()
+        msg = "checked_object is not an integer nor a float"
+        self.assertIsInstance(checked_object, NormInstances, msg)
 
 ######################### tests #########################
 
