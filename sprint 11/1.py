2972b20c8e04fcc994880eb5d0104486ec9d543e
Iryna Pleshyvtseva <superfrench041@gmail.com>
2022-08-18 14:27:03 +0300

    added updated sprint 11

--- sprint 11/1.py
+++ sprint 11/1.py
@@ -1,16 +1,3 @@
-rows = [(3002, 'Nick Rimando', 'New York', 100, 5001),
-(3005, 'Graham Zusi', 'California', 200, 5002),
-(3001, 'Brad Guzan', 'London', 100, 5005),
-(3004, 'Fabian Johns', 'Paris', 300, 5006),
-(3007, 'Brad Davis', 'New York', 200, 5001),
-(3009, 'Geoff Cameron', 'Berlin', 100, 5003),
-(3008, 'Julian Green', 'London', 300, 5002),
-(3003, 'Jozy Altidore', 'Kyiv', 200, 5007)]
-
-for row in rows:
-    if row[0] == 3002:
-        print(True)
-
 import sqlite3 as lite
 import sys
 
@@ -18,20 +5,25 @@ con = lite.connect("q1.db")
 
 with con:
     cur = con.cursor()
-    cur.execute("SELECT * FROM customers")
+    cur.execute("SELECT * FROM customers WHERE grade > 200 ORDER BY Id")
     rows = cur.fetchall()
     
-    print("Connected to SQLite", "Total rows are:   2", "Printing each row", sep="\n")
+    print(f"Connected to SQLite \nTotal rows are:   {len(rows)} \nPrinting each row")
     for row in rows:
-        if row[0] == 3004 and row[1] == "Fabian Johns" and row[2] == "Paris" and row[3] == 300 and row[4] == 5006:
-            print(f"Id:  {row[0]} \nName:  {row[1]} \nCity:  {row[2]} \nGrade:  {row[3]} \nSeller:  {row[4]}")
-    print("", "", sep="\n")
-    for row in rows:
-        if row[0] == 3008 and row[1] == "Julian Green" and row[2] == "London" and row[3] == 300 and row[4] == 5002:
-            print(f"Id:  {row[0]} \nName:  {row[1]} \nCity:  {row[2]} \nGrade:  {row[3]} \nSeller:  {row[4]}")
+        print(f"Id:  {row[0]} \nName:  {row[1]} \nCity:  {row[2]} \nGrade:  {row[3]} \nSeller:  {row[4]}")
+        print("", "", sep="\n")
+    
+    # print("Connected to SQLite", "Total rows are:   2", "Printing each row", sep="\n")
+    # for row in rows:
+    #     if row[0] == 3004 and row[1] == "Fabian Johns" and row[2] == "Paris" and row[3] == 300 and row[4] == 5006:
+    #         print(f"Id:  {row[0]} \nName:  {row[1]} \nCity:  {row[2]} \nGrade:  {row[3]} \nSeller:  {row[4]}")
+    # print("", "", sep="\n")
+    # for row in rows:
+    #     if row[0] == 3008 and row[1] == "Julian Green" and row[2] == "London" and row[3] == 300 and row[4] == 5002:
+    #         print(f"Id:  {row[0]} \nName:  {row[1]} \nCity:  {row[2]} \nGrade:  {row[3]} \nSeller:  {row[4]}")
 
 cur.close()
 con.close()
 
-print("", "", sep="\n")
+# print("", "", sep="\n")
 print("The SQLite connection is closed")
